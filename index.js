2dcfbf62bda552345d1cda36561c21de27d9dedd
Anisul Islam <anis.inbox10@gmail.com>
2025-05-23 15:37:51 +0500

    vercel

--- index.js
+++ index.js
@@ -147,10 +147,10 @@ app.use(errorConverter);
 // Handle error
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV || "development"} mode on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// const server = app.listen(PORT, () => {
+//   console.log(`Server running in ${process.env.NODE_ENV || "development"} mode on port ${PORT}`);
+// });
 
 // Handle unhandled promise rejections
 process.on("unhandledRejection", (err) => {
